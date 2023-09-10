0490ea47353ed1a41322699b1b3113f0a8bb4f8c
saro <netsv1643+gi@gmail.com>
2023-09-11 08:46:34 +0900

    変数名を一部変更・ボタンとラベルの配置を逆に配置・ボタンの色を変更

--- free-memo.py
+++ free-memo.py
@@ -2,36 +2,46 @@ import tkinter as tk
 import tkinter as t
 from tkinter.scrolledtext import ScrolledText
 
-imput_word = 0
+word_count = 0
 
 
 def entry_count():
-    global input_word
-    input_word = entry.get(0.0, t.END)
-    text_count["text"] = len(input_word) - 1
-    window.after(1000, entry_count)
+    global word_count
+    word_count = entry.get(0.0, t.END)
+    count_label["text"] = len(word_count) - 1
+    root.after(1000, entry_count)
 
 
 # メインウィンドウ作成
-window = tk.Tk()
-window.title("フリーメモ")
-window.geometry("600x200")
+root = tk.Tk()
+root.title("フリーメモ")
+root.geometry("600x220")
 
 # テキストフレーム・テキストボックス作成
-text_frame = tk.Frame(window, width=500, height=100, bg="#ff0000")
+text_frame = tk.Frame(root, width=500, height=100)
 text_frame.pack()
 entry = ScrolledText(text_frame, font=("メイリオ", 14), height=5, width=40)
 entry.pack()
 
 # 送信・文字数カウントラベル作成
-frame = tk.Frame(window)
+frame = tk.Frame(root, width=500, height=30)
 frame.pack(padx=20, pady=10)
 
-send_button = tk.Button(frame, text="送信", width="50")
-send_button.grid(row=0, column=0)
-
-text_count = tk.Label(frame, text=0, width="20", font=("メイリオ", 14))
+count_label = tk.Label(frame, text=0, width="10", font=("BIZ UDゴシック", 14))
 entry_count()
-text_count.grid(row=0, column=1)
+count_label.grid(row=0, column=0)
 
-window.mainloop()
+send_button = tk.Button(
+    frame,
+    text="送信",
+    width="35",
+    pady=5,
+    fg="#ffffff",
+    bg="#1E88E5",
+    relief=tk.FLAT,
+    font=("BIZ UDゴシック", 14),
+    anchor="center",
+)
+send_button.grid(row=0, column=1)
+
+root.mainloop()
